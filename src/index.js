dd42b81be662005695068bbdcd2970e1117bffe6
vladiks-git <ershovvlad18@yandex.ru>
2021-02-24 15:49:47 +0300

    'first'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0)
+     return Number((n*-1).toString().split('').reverse().join(''))
+    return Number(n.toString().split('').reverse().join(''))
 }
